8d930508ead7b46b56ea47a4347ff922bb22677f
wenxiaofang2022 <wenxiaofang.jiayou@gmail.com>
2022-08-01 10:17:27 +0800

    fix:parallax完成

--- js/vendor/jquery.parallax.js
+++ js/vendor/jquery.parallax.js
@@ -9,55 +9,34 @@
 	$.fn.parallax = function(ypos,outerDiv){
 		var $this = $(this);
 		var firstTop;
-		var firstLeft;
-		var getHeight = function(jqo) {
-			return jqo.height();
-		};
+		var _top;
 
 		if (arguments.length < 1 || ypos === null) ypos = 0;
-		// if (arguments.length < 2 || xpos === null) xpos = 0;
 		if (arguments.length < 2 || outerDiv === null) outerDiv = 'body';
 
 		$this.each(function(){
 		  firstTop = $this.offset().top;
-			staticTop = parseInt($this.css('top'));
-			//firstLeft = parseInt($this.css('left'));
+			var _transform = $this.css("transform");
+			var matrixArr = _transform.split(',');
+			_top = parseInt(matrixArr[5]);
 		});
 
 		function update(){
-			//var preScrollTop = Number(sessionStorage.getItem("preScrollTop"));
 			var pos_y = $window.scrollTop();
-			// var distance = preScrollTop - pos_y;
-			// sessionStorage.setItem("preScrollTop",pos_y);
-			// var pos_x = 10;
+			var t_y = $(outerDiv).offset().top;
 			$this.each(function(){
 				var $element = $(this);
-				var top = $element.offset().top;
-				var height = getHeight($element);
-
-				if (top + height < pos_y || top > pos_y + windowHeight) {
+				var _transform = $element.css("transform");
+				var matrixArr = _transform.split(',');
+				if((pos_y - t_y < 0) || (pos_y - t_y == 0)){
 					return;
 				}
 
 				if(ypos===0)return;
-				console.log("staticTop",staticTop);
-				console.log("firstTop",firstTop);
-				console.log("pos_y",pos_y);
-				console.log("height",height);
-				// console.log("distance",distance);
-				//var _top = Math.round(Math.round((firstTop - pos_y) * ypos));
-				// if(distance>0){
-				// 	console.log("yemian222--页面向上移动--看到下面的内容");
-				// 	// _top = Math.ceil(parseInt($this.css('top')) + distance*ypos)
-				// }
-				// else if(distance<0){
-				// 	console.log("yemian111--页面向下移动--看到上面的内容");
-				// 	// _top = Math.ceil(parseInt($this.css('top')) + distance*ypos)
-				// }
-				var _top = Math.round(Math.round((firstTop - pos_y) * ypos));
-				// $this.css('top',_top + "px");
-				$this.css('transform','translateY(' +_top + 'px)');
-				$this.addClass('active');
+				var _left = parseInt(matrixArr[4]);
+
+				var t_top = Math.round(Math.round((_top - (pos_y - t_y)) * ypos));
+				$this.css('transform','translate('+_left+'px,'+t_top+'px)');
 			});
 		}
 
